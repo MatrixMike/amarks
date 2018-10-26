65610e7bbfb88b1b71c1d0ac033ae7a9efd91899
Mike Hewitt <matroxmike@icloud.com>
2018-10-26 11:06:53 +1100

    minor change
    just revive

--- src/helloworld/core.clj
+++ src/helloworld/core.clj
@@ -1,12 +1,12 @@
 ;; (ns amarks.core)
-
+(comment revived Thursday, 25 October 2018 )
 (ns helloworld.core ; namespace def’n
 
 (:gen-class))     ; compilation directive
 
 (defn helloworld
 []
-"Hello, world 2")      ; static function
+"Hello, world 3")      ; static function
 
 (defn -main   
      "Entrypoint"           ; docstring  ;; the quotes used in ppt are not OK when copied into source file
